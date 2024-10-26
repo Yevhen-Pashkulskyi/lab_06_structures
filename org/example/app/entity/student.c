ab5c656b5095906512c05b7845735bcb2b2b39b5
Yevhen-Pashkulskyi <pashkulskyevhen@gmail.com>
2024-10-26 11:03:51 +0300

    modified student

---
+++ org/example/app/entity/student.c
@@ -0,0 +1,11 @@
+struct Student {
+    char name[50];
+    char surname[50];
+    char patronymic[50];
+    int year_of_university;
+};
+struct Semester {
+    float math;
+    float physics;
+    float programming_or_drawing;
+};
