5da58af8932cc319fcc643fa30a10c92b2cf24c0
Yevhen-Pashkulskyi <pashkulskyevhen@gmail.com>
2024-11-15 23:34:55 +0200

    add view menu and switch

--- org/example/app/controll/appController.c
+++ org/example/app/controll/appController.c
@@ -3,18 +3,35 @@
 #include <stdlib.h>
 #include <time.h>
 
+void viewMenu() {
+    printf("1. Start program"
+        "\n0. Stop program\n"
+        "\nEnter your choice:");
+}
 
 int run() {
     srand(time(NULL));
     int size = random_num(1, 5);
-    printf("\nsize students: %d \n", size);
-    // scanf("\n%d", &size);
-    // getchar();
-    struct Student *students = created_Student(size);
-    if (students != NULL) {
-        print(students, size);
-        free(students);
+    viewMenu();
+    int choice;
+    scanf("%d", &choice);
+    switch (choice) {
+        case 1:
+            printf("\nsize students: %d \n", size);
+            fflush(stdin);
+            struct Student *students = created_Student(size);
+            if (students != NULL) {
+                print(students, size);
+                free(students);
+            }
+            run();
+            break;
+        case 0:
+            break;
+        default: printf("Invalid choice\n");
+            fflush(stdin);
+            run();
     }
-    run();
+
     return 0;
 }
